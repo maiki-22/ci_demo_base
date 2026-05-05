d29c73cac0cd99c2ac79bc4aa322b02cdd08991e
Miguel Cortés Sandoval <miguel.cortes.sandoval23@gmail.com>
2026-05-05 11:41:08 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const sum = require('./sum');
  function testSum() {
-   const result = sum(2,5)
+   const result = sum(2,3)
 
    if (result !== 5) {
      throw new Error(`no es igual a 5 :c, se obtutvo ${result}`);
